cc07efb9a433fe18860247e5e441eaedc5f4b2ad
arslantu <arslantu@arslantu.xyz>
2023-07-31 13:19:04 +0800

    feat✨: add load function to load sharegpt dataset

--- utils/dataset_utils.py
+++ utils/dataset_utils.py
@@ -4,6 +4,48 @@ from datasets import load_dataset
 
 logger = logging.getLogger("StreamLogger")
 
+def load_sharegpt_20230401_clean_lang_split_126k(
+        tokenizer,
+        data_path: str,
+        max_len=2048,
+        num_proc: int=1):
+    logger.debug(f"Load , sharegpt_20230401_clean_lang_split_126k, max_len: {max_len}, num_proc: {num_proc}")
+    def tokenize(prompt, add_eos_token=True):
+        result = tokenizer(
+            prompt,
+            truncation=True,
+            max_length=max_len,
+            padding=False,
+            return_tensors=None,
+        )
+        if (
+                result["input_ids"][-1] != tokenizer.eos_token_id
+                and len(result["input_ids"]) < max_len
+                and add_eos_token
+        ):
+            result["input_ids"].append(tokenizer.eos_token_id)
+            result["attention_mask"].append(1)
+
+        if add_eos_token and len(result["input_ids"]) >= max_len:
+            result["input_ids"][max_len - 1] = tokenizer.eos_token_id
+            result["attention_mask"][max_len - 1] = 1
+
+        result["labels"] = result["input_ids"].copy()
+        return result
+
+    def generate_and_tokenize_prompt(data_point):
+        # TODO: use multi-turn conversation
+        input_text = data_point['conversation'][0]['value']
+        input_text = "<|User|>:" + input_text + "<eoh>\n<|Bot|>:"
+        target_text = data_point['conversation'][1]['value'] + "<eoa>\n"
+        full_prompt = input_text + target_text
+        tokenized_full_prompt = tokenize(full_prompt)
+        return tokenized_full_prompt
+
+    data = load_dataset("json", data_files=data_path, split='train')
+    data = data.map(generate_and_tokenize_prompt, num_proc=num_proc)
+    return data
+
 def load_belle_open_source_500k(
         tokenizer, 
         data_path: str, 
